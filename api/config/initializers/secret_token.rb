ea07a8302d661f128bfaf255f2750dcba022b59d
Ben Vandgrift <ben@vandgrift.com>
2011-05-14 11:38:21 -0400

    adding a readme and a rails api
     - use rvm or check ruby version
     - requires bundler

---
+++ api/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Api::Application.config.secret_token = '12d9707147950e82eacf9e2e7dfb97314b74706c6c46172d03eb4c5b099d7f21d5ade7389c2d98f9b70ec1b3b454c463285d5e9ee76fd3854ca3ff5202615537'
